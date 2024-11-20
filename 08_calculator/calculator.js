368a49f6cac7ccdef5365a06f5ecd017f25b0c1b
HM-Jubaer <hasanmabudjuber@gmail.com>
2024-11-20 14:01:44 +0600

    updated Answer

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+  let sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  let subtract = a - b;
+  return subtract;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let sumArray = a.reduce(function (accumulator, current) {
+    return accumulator + current;
+  }, 0);
+
+  return sumArray;
 };
 
-const multiply = function() {
+const multiply = function(a) {
+  let multiply = a.reduce(function (accumulator, current) {
+    return accumulator * current;
+  }, 1);
 
+  return multiply;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1; 
+  for (let i = 1; i <= number; i++) {
+    result *= i; 
+  }
+
+  return result; 
 };
 
 // Do not edit below this line
